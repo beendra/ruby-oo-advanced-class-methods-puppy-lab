37b3aec0cb4dac88a7bc4822762fdf4060b50ccc
beendra <alexandra.ricardel@gmail.com>
2021-04-15 21:00:36 -0500

    adding

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.map { |dog| dog.name }
+        #couldn't get this to pass because i forgot to add the attr method. remember to double check in the future!!
+    end
+
+    def save
+        @@all << self
+    end
+    
+
+end
